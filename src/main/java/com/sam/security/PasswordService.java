da0f5754c68abd663c4031dbade7c45e96f53f83
sam2692 <deshmukhganpat2626@gmail.com>
2024-01-14 23:23:30 +0530

    j var added

--- src/main/java/com/sam/security/PasswordService.java
+++ src/main/java/com/sam/security/PasswordService.java
@@ -7,6 +7,7 @@ import java.util.Base64.Encoder;
 public class PasswordService {
 
 	int i=10;
+        int j=20;
 	public static String encode(String txt)
 	{
 		Encoder encoder = Base64.getEncoder();
